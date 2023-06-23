b8df84e52c8c6254b649eb52d52654dbc8f6f376
Anish Rai <engg.anishrai@gmail.com>
2023-06-23 11:26:33 +0530

    Adding a new java file Add.java

---
+++ Add.java
@@ -0,0 +1,10 @@
+public class Add
+{
+    public static void main(String [] args)
+    {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Addition of Both Numbers = "a+b);
+    }
+}
